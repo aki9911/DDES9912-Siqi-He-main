65d5d6e8b225569dccdc39572e06989a5a9635b4
aki9911 <z5545057@ad.unsw.edu.au>
2025-10-17 01:28:40 +1100

    fix

--- Assets/__My Project/scrip/spin.cs
+++ Assets/__My Project/scrip/spin.cs
@@ -2,7 +2,14 @@ using UnityEngine;
 
 public class NewMonoBehaviourScript : MonoBehaviour
 {
-    public float zSpeed;
+    public float zSpeed=360f;
+    public Transform pencil;
+    public float moveSpeed=0.05f;
+    public float moveDistance=0.2f;
+
+    private float movedDistance=0f;
+    private bool isMoving = false;
+    private bool isRotating = false;
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
@@ -12,6 +19,37 @@ public class NewMonoBehaviourScript : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        transform.Rotate(0,0,zSpeed * Time.deltaTime);
+    
+        if (isRotating)
+        {
+        transform.Rotate(0f,0f,zSpeed * Time.deltaTime);
+        }
+       
+
+        if(pencil != null && isMoving)
+        {
+            float moveStep = moveSpeed *  Time.deltaTime;
+            pencil.Translate(0f,0f,moveStep,Space.Self);
+            movedDistance +=moveStep;
+
+            if (movedDistance>= moveDistance)
+            {
+                isMoving = false;
+            }
+        }
     }
+    void OnMouseDown()
+    {
+        isRotating =!isRotating;
+        if(isRotating)
+        {
+            isMoving=true;
+            movedDistance=0f;
+        }
+        else
+        {
+            isMoving=false;
+        }
+    }
+
 }
