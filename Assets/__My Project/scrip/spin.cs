4005d2ecd8da0e1292bc3eacfab8a7b1ba9575a6
aki9911 <z5545057@ad.unsw.edu.au>
2025-10-17 02:39:07 +1100

    fixed

--- Assets/__My Project/scrip/spin.cs
+++ Assets/__My Project/scrip/spin.cs
@@ -1,34 +1,44 @@
 using UnityEngine;
 
 public class NewMonoBehaviourScript : MonoBehaviour
+    
 {
-    public float zSpeed=360f;
-    public Transform pencil;
-    public float moveSpeed=0.05f;
-    public float moveDistance=0.2f;
-
-    private float movedDistance=0f;
-    private bool isMoving = false;
-    private bool isRotating = false;
+   public float zSpeed;
+   public Transform pencil;
+   public float moveSpeed;
+   public float moveDistance;
+    
+   private float movedDistance;
+   private bool isMoving = false;
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
         
     }
+   public void pencilSpeed(float nSpeed)
+   {
+    moveSpeed = nSpeed;
+   }
+   public void pencilDistance(float dSeep)
+   {
+    moveDistance = dSeep;
+   }
+    public void StartPencilMove()
+   {
+    movedDistance = 0f; 
+    isMoving = true;  
 
+   }
     // Update is called once per frame
     void Update()
-    {
+    { 
+     transform.Rotate(0f,0f,zSpeed * Time.deltaTime);
+        
     
-        if (isRotating)
-        {
-        transform.Rotate(0f,0f,zSpeed * Time.deltaTime);
-        }
-       
 
         if(pencil != null && isMoving)
         {
-            float moveStep = moveSpeed *  Time.deltaTime;
+            float moveStep = moveSpeed * Time.deltaTime;
             pencil.Translate(0f,0f,moveStep,Space.Self);
             movedDistance +=moveStep;
 
@@ -38,18 +48,10 @@ public class NewMonoBehaviourScript : MonoBehaviour
             }
         }
     }
-    void OnMouseDown()
-    {
-        isRotating =!isRotating;
-        if(isRotating)
-        {
-            isMoving=true;
-            movedDistance=0f;
-        }
-        else
-        {
-            isMoving=false;
-        }
-    }
 
+   public void SetSpeed(float newSpeed)
+    {
+        zSpeed = newSpeed;
+    }
+  
 }
