a4e5e69ec9aa52379723e078f3030f53baddbec0
aki9911 <z5545057@ad.unsw.edu.au>
2025-10-17 18:23:27 +1100

    replace

--- Assets/__My Project/scrip/spin.cs
+++ Assets/__My Project/scrip/spin.cs
@@ -7,13 +7,19 @@ public class NewMonoBehaviourScript : MonoBehaviour
    public Transform pencil;
    public float moveSpeed;
    public float moveDistance;
+
+   public GameObject oldTip;
+   public GameObject newTip;
     
    private float movedDistance;
    private bool isMoving = false;
+   private bool isReturning = false;
+   private bool tipReplaced = false;
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
-        
+        if(oldTip!=null)oldTip.SetActive(true);
+        if(newTip!=null)newTip.SetActive(false);
     }
    public void pencilSpeed(float nSpeed)
    {
@@ -26,9 +32,18 @@ public class NewMonoBehaviourScript : MonoBehaviour
     public void StartPencilMove()
    {
     movedDistance = 0f; 
-    isMoving = true;  
-
+    isMoving = true;
+    isReturning = false;
+    tipReplaced = false;  
    }
+    public void Stop()
+    {
+      isMoving = false;
+      if(movedDistance>0f)
+      {
+        isReturning = true;
+      }
+    }
     // Update is called once per frame
     void Update()
     { 
@@ -45,13 +60,38 @@ public class NewMonoBehaviourScript : MonoBehaviour
             if (movedDistance>= moveDistance)
             {
                 isMoving = false;
+                ReplaceTip();
             }
+        }
+        else if (isReturning)
+        {
+            float moveStep = moveSpeed *  Time. deltaTime;
+            if(moveStep>movedDistance)
+               moveStep = moveDistance;
+
+             pencil.Translate(0f,0f,-moveStep,Space.Self);
+             movedDistance-= moveStep;
+             if (movedDistance<=0f) 
+             {
+                movedDistance = 0f;
+                isReturning =   false;
+             } 
         }
     }
 
    public void SetSpeed(float newSpeed)
     {
         zSpeed = newSpeed;
+    }
+
+    private void ReplaceTip()
+    {
+        if(tipReplaced)return;
+
+        if(oldTip!=null)oldTip.SetActive(false);
+        if(newTip!=null)newTip.SetActive(true);
+
+        tipReplaced=true;    
     }
   
 }
