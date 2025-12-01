6e4ce847a88cf8eeb123581af833092e51981b64
aki9911 <z5545057@ad.unsw.edu.au>
2025-12-01 15:09:37 +0800

    NPC

---
+++ Assets/__My Project/scrip/NPCwalk.cs
@@ -0,0 +1,21 @@
+using UnityEngine;
+using UnityEngine.AI;
+
+[RequireComponent (typeof(NavMeshAgent))]
+public class NPCwalk : MonoBehaviour
+{
+    public NavMeshAgent myAgent;
+    public Transform destination;
+
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        myAgent = GetComponent<NavMeshAgent>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        myAgent.destination = destination.position;
+    }
+}
