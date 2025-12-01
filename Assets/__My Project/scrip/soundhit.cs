f81d6208c37399136c5968a81c02d72918bf6c69
aki9911 <z5545057@ad.unsw.edu.au>
2025-12-01 19:47:53 +0800

    final

---
+++ Assets/__My Project/scrip/soundhit.cs
@@ -0,0 +1,21 @@
+using UnityEngine;
+
+public class soundhit : MonoBehaviour
+{
+    private AudioSource audioSource;
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        audioSource = GetComponent<AudioSource>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+    private void OnCollisionEnter(Collision collision)
+    {
+        audioSource.Play();
+    }
+}
